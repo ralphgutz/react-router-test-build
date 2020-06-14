fcd5dc23973b1ae144a0c89b3469eb1cc337fc9c
Raphael Gutierrez <urd1_17@yahoo.com>
2020-06-14 19:13:39 +0800

    Push to github

---
+++ precache-manifest.72d07ef1296e119b8cc15d24f406af14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d82eab5d7f8928ab6d503a8aae7058",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8351bf677852584d7c19",
+    "url": "/static/css/main.249cd6e0.chunk.css"
+  },
+  {
+    "revision": "c09522480cad6213e10c",
+    "url": "/static/js/2.f0c02ba2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f0c02ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8351bf677852584d7c19",
+    "url": "/static/js/main.e95ec185.chunk.js"
+  },
+  {
+    "revision": "318d1e83f6d41f863feb",
+    "url": "/static/js/runtime-main.2d594f31.js"
+  }
+]);
